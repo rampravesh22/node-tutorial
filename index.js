8973b15a421e7e405a25064a045b99cadaf33649
rpc1752 <rpc1752@gmail.com>
2023-09-06 09:51:39 +0530

    findal updated

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const products = data.products;
 server.use(express.json());
 
 // end points routes
-server.get("/product/:id", auth, (req, res) => {
+server.get("/product/:id", (req, res) => {
 	res.send(req.body);
 });
-server.post("/products", auth, (req, res) => {
+server.post("/products", (req, res) => {
 	res.send("post completed");
 });
-server.put("/delete-products/:id", auth, (req, res) => {
+server.put("/delete-products/:id", (req, res) => {
 	res.send("put");
 });
-server.delete("/", auth, (req, res) => {
+server.delete("/", (req, res) => {
 	res.send("delete");
 });
-server.patch("/", auth, (req, res) => {
+server.patch("/", (req, res) => {
 	res.send("patch");
 });
 
