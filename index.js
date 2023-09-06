7bfde0b50186792244373103828ce2c78579f579
rpc1752 <rpc1752@gmail.com>
2023-09-06 09:19:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+// end points routes
+server.get("/product/:id", auth, (req, res) => {
+	res.send(req.body);
+});
+server.post("/products", auth, (req, res) => {
+	res.send("post completed");
+});
+server.put("/delete-products/:id", auth, (req, res) => {
+	res.send("put");
+});
+server.delete("/", auth, (req, res) => {
+	res.send("delete");
+});
+server.patch("/", auth, (req, res) => {
+	res.send("patch");
+});
+
+server.listen(8080, () => {
+	console.log("server running...");
+});
