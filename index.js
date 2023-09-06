4afdad5a6ac2c699ef447b6ce242676926926993
rpc1752 <rpc1752@gmail.com>
2023-09-06 09:28:21 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const server = express();
 
 server.use(express.json());
+server.use(morgan("default"));
+server.use(express.static("public"));
 
 // end points routes
 server.get("/product/:id", auth, (req, res) => {
