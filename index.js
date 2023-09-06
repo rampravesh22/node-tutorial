378007bb084312333633e3ebef4e33e6b856edea
rpc1752 <rpc1752@gmail.com>
2023-09-06 09:52:29 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const morgan = require("morgan");
+const fs = require("fs");
 const server = express();
-
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 server.use(express.json());
-server.use(morgan("default"));
-server.use(express.static("public"));
 
 // end points routes
-server.get("/product/:id", auth, (req, res) => {
+server.get("/product/:id", (req, res) => {
 	res.send(req.body);
 });
-server.post("/products", auth, (req, res) => {
+server.post("/products", (req, res) => {
 	res.send("post completed");
 });
-server.put("/delete-products/:id", auth, (req, res) => {
+server.put("/delete-products/:id", (req, res) => {
 	res.send("put");
 });
-server.delete("/", auth, (req, res) => {
+server.delete("/", (req, res) => {
 	res.send("delete");
 });
-server.patch("/", auth, (req, res) => {
+server.patch("/", (req, res) => {
 	res.send("patch");
 });
 
