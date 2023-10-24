80d5b49a43c5970949d1286e6bef8b08de138219
rpc1752 <rpc1752@gmail.com>
2023-10-24 10:42:14 +0530

    updated

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const server = express();
 
 server.use(express.json());
 // end points routes
-server.get("/product/:id", (req, res) => {
-	res.send(req.body);
-});
-server.post("/products", (req, res) => {
-	res.send("post completed");
-});
-server.put("/delete-products/:id", (req, res) => {
-	res.send("put");
-});
-server.delete("/", (req, res) => {
-	res.send("delete");
-});
-server.patch("/", (req, res) => {
-	res.send("patch");
-});
 
 server.listen(8080, () => {
 	console.log("server running...");
