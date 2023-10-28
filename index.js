016c2fc2b5f0c8237b2570fa3deee4133101d20a
rpc1752 <rpc1752@gmail.com>
2023-10-28 12:16:33 +0530

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const server = express();
-
-server.use(express.json());
-// end points routes
-
-server.listen(8080, () => {
-	console.log("server running...");
-});
