0c8bb6238d483fc7e88a8c7c3bba121d2d2dc2bb
rpc1752 <rpc1752@gmail.com>
2024-09-15 11:22:31 +0530

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
 const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
 dotenv.config();
+app.use(express.json());
+app.use(cors());
 
-const getData = async () => {
-	// Make sure to include these imports:
-	// import { GoogleGenerativeAI } from "@google/generative-ai";
-	const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-	const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+app.get("/gemini", async (req, res) => {
+	try {
+		const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-	const prompt = "what is python.";
+		const prompt = req.query.query;
 
-	const result = await model.generateContent(prompt);
-	console.log(result.response.text());
-};
+		const result = await model.generateContent(prompt);
+		res.json({ message: result.response.text(), test: "data" }).status(201);
+	} catch (error) {
+		res.json(error).status(400);
+	}
+});
 
-getData();
+app.listen(3000, () => {
+	console.log("running on port 3000");
+});
